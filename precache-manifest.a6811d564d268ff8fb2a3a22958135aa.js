a6737f974cf5602b75c0dcd18cb898f1f7c5dd2d
shivu <venkateshkonuku888@gmail.com>
2020-06-22 21:01:47 +0530

    Updates

---
+++ precache-manifest.a6811d564d268ff8fb2a3a22958135aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47588ed58781ec9fabad1da5b69f7fb",
+    "url": "/project-1/index.html"
+  },
+  {
+    "revision": "10e71889da5c46e8cec9",
+    "url": "/project-1/static/css/main.2043e724.chunk.css"
+  },
+  {
+    "revision": "c1109b6d9430ea513df3",
+    "url": "/project-1/static/js/2.495d87a9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/project-1/static/js/2.495d87a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10e71889da5c46e8cec9",
+    "url": "/project-1/static/js/main.d8095fee.chunk.js"
+  },
+  {
+    "revision": "9f7afaa6185b2b4db7f1",
+    "url": "/project-1/static/js/runtime-main.5706c51f.js"
+  }
+]);
